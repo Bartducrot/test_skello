ee0f75265f7cd6a66a0649418a1ed125d734ba69
Barthelemy Ducrot <barthelemy.ducrot@hei.fr>
2017-10-18 22:41:52 +0200

    add basic view for the post index

--- app.rb
+++ app.rb
@@ -1,7 +1,14 @@
 require "sinatra"
 require "sinatra/reloader" if development?
+require "pry-byebug"
+require "better_errors"
 require_relative "database"
+configure :development do
+  use BetterErrors::Middleware
+  BetterErrors.application_root = File.expand_path('..', __FILE__)
+end
 
 get "/" do
   @database = DB
+  erb :index
 end
